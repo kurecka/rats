e6c92aa473aad7d8e4a66f3c805eabdb57afd396
Václav Nevyhoštěný <xnevyhos@fi.muni.cz>
2024-03-25 17:08:48 +0100

    sync

---
+++ sync_rats_docker.sh
@@ -0,0 +1,7 @@
+rm -rf docker_build
+mkdir -p docker_build
+cd docker_build && \
+git clone git@gitlab.fi.muni.cz:risk-aware-decision-making/rats.git -b exact_lp
+docker cp docker_build/rats rats-app:/work/rats
+docker exec rats-app bash -c "cd /work/rats && export CMAKE_COMMON_VARIABLES=-DPEDANTIC=OFF && pip install ."
+rm -rf docker_build

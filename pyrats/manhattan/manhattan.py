cabd8c44c1748d9f53776203dac81cb780c4fa51
uncovsky <vit.uncovsky@gmail.com>
2024-06-23 20:11:37 +0200

    feat(manhattan): rework manhattan penalties, periods. Add comments and setup script.

--- pyrats/manhattan/manhattan.py
+++ pyrats/manhattan/manhattan.py
@@ -3,57 +3,55 @@ import copy
 from fimdpenv import AEVEnv
 import folium
 from itertools import islice
-from math import asin, cos, sqrt, pi
+from math import asin, cos, sqrt, pi, inf
 
 import numpy as np
 import networkx as nx
 import time
 
-# defaults from their implementation
-targets = ['42440465','42445916']
-
 # Calculates distance in km from latitude and longitude of two points using
-# the Haversine formula, 
+# the Haversine formula,
 # see: https://stackoverflow.com/questions/27928/calculate-distance-between-two-latitude-longitude-points-haversine-formula
 def distance_from_gps(long1, lat1, long2, lat2):
     R = 6371
-    p = pi / 180 
-    a = 0.5 - cos((lat2-lat1)*p)/2 + cos(lat1*p) * cos(lat2*p) * \
-                  (1-cos((long2-long1)*p))/2
+    p = pi / 180
+    a = 0.5 - cos((lat2-lat1)*p)/2 + cos(lat1*p) * cos(lat2*p) * (1-cos((long2-long1)*p))/2
     return 2 * R * asin(sqrt(a))
 
-# states - labels (json), see above 
-# actions - integers, (see variable aid in fimdp/core.py)
-# S - state_label, state_of_targets, decision_node
-# A - index size_t
+
 class ManhattanEnv:
 
     """
-        capacity - starting capacity of the agent
-        targets - the reachability targets, currently receive unit reward for
-            hitting a state in targets
-        init_state - initial state  (json label), randomize if none
+        Manhattan MDP environment simulator built on top of FiMDPEnv environment AEVEnv.
+        The agent moves through an area of Manhattan from 42nd to 116th street and receives orders from
+        various customers. The goal of the agent is to maximize finished orders (rewards) while avoiding
+        delayed deliveries (penalty).
 
-        self.energy / self.capacity -> curr/max energy
+        targets - labels of reachability targets / states of the mdp that provide orders for the agent
 
-        self.history -> tracks data that is recorded before calling step()
-            i.e. S = (state, state_of_targets, decision_node, energy)
+        init_state - initial state of the agent
 
-        periods -> dict of target + int/float
+        period - the cooldown on each customer order.
 
-        cons_thd = threshold consumption to receive penalty after accepting order
+        capacity - the consumption capacity of the agent. Picking a direction results in a stochastic energy consumption
+                   taken from the original AEVEnv benchmark. See https://arxiv.org/abs/2005.07227 - section 7.1.
+                   This consumption is then subtracted from the agents capacity as well as from the periods.
+                   Once the agent reaches an energy level of zero, the
+                   environment terminates, passing capacity = 0 results in a
+                   non-terminating environment
 
-        MDP state consists of the current state, the counters for each target, and decision node indicator
+        cons_thd - the delay threshold on the orders, each cons_thd fuel consumed during an active order incurs a penalty of 1
+                   on the agent.
+
+        radius - the distance limit on accepting orders in kilometers
     """
-    def __init__(self, targets, init_state, periods, capacity, cons_thd=10.0, radius=2.0):
+    def __init__(self, targets, init_state, period, capacity, cons_thd=10.0, radius=2.0):
         self.env = AEVEnv.AEVEnv(capacity, targets, [], init_state,
-                datafile="manhattan_res/NYC.json",
-                mapfile ="manhattan_res/NYC.graphml")
+                datafile="/work/rats/rats/manhattan_res/NYC.json",
+                mapfile ="/work/rats/rats/manhattan_res/NYC.graphml")
 
-        # maps int -> ( state, energy )
+        # maps checkpoint ids to state and history information
         self.checkpoints = dict()
-        # maps int -> histories
-        self.histories = dict()
 
         if not init_state:
             # randomize starting state
@@ -63,25 +61,32 @@ class ManhattanEnv:
 
         self.position = self.init_state
 
-        # last gps position
+        # last known gps position, used when evaluating distance from targets
         self.last_gps = None
 
-        self.capacity = capacity
         self.energy = capacity
+        self.capacity = capacity
+
+        self.cons_thd = cons_thd
+        self.order_delay = 0
 
         self.targets = targets
-        self.periods = periods
+        self.period = period
         self.radius = radius
-        self.cons_thd = cons_thd
 
         # decision node -> special part of state (flag) - signals that orders
         # can be accepted at this step
         self.decision_node = False
 
         # ctr for each target, -1 is active target (accepted order)
-        self.state_of_targets = { t : self.periods[t] for t in self.targets }
+        self.state_of_targets = { t : period for t in self.targets }
 
+        # history of positions, used to animate the trajectory of the agent
         self.history = []
+
+        # maps indices to lengths of the history, used to remove simulated actions/states from history
+        # when restoring checkpoints
+        self.histories = {}
 
         # graph with latitude and longitude information, used to evaluate
         # distance from targets and plot an animation of the trajectory.
@@ -91,8 +96,11 @@ class ManhattanEnv:
 
     """
         helper methods
+
+        used mainly for converting between state representations of AEVEnv
+        and this class
     """
-    # translate names (json labels) to cmdp states used in functions
+    # translate state names (json labels) to cmdp states used in AEVEnv
     def name_to_state(self, name):
         return self.env.consmdp.names_dict[name]
 
@@ -100,12 +108,20 @@ class ManhattanEnv:
     def state_to_name(self, state):
         return self.env.state_to_name[state]
 
-    # generate a random state (TODO: use for delivery targets)
-    # FIXME: dummy states for actions can be generated by this, probably not
-    # what we want
+    # generate a random state
+    # TODO: can generate dummy states this way, which results in unwanted behavior
+    # however, we do not use this method in any benchmarks as of now.
     def random_state(self):
         return self.state_to_name(np.random.choice(self.env.consmdp.num_states))
 
+    def target_active(self, target):
+        return self.state_of_targets[target] == -1
+
+    def currently_delivering(self):
+        for t in self.targets:
+            if self.target_active(t):
+                return True
+        return False
     """
         env interface methods
     """
@@ -120,7 +136,7 @@ class ManhattanEnv:
         get the number of actions for the current state
         if in decision node, return indices to active targets
 
-        -1 signals refusing any orders
+        -1 signals refusing any available orders.
     """
     def get_actions_for_state(self, name):
         # able to accept orders
@@ -134,10 +150,8 @@ class ManhattanEnv:
     def current_state(self):
         return self.position, self.state_of_targets, self.decision_node
 
-    # c++ state is a tuple of (state_name, state_of_targets, decision_node)
     def possible_actions(self, state = None):
-
-        # if called with deafult init state tuple (from c++) or None,
+        # if called with default init state tuple (from c++) or None,
         # get actions for current position
         if state is None or state[0] == '':
             state_name = self.position
@@ -145,44 +159,33 @@ class ManhattanEnv:
             state_name = state[0]
         return self.get_actions_for_state(state_name)
 
-    def target_active(self, target):
-        return self.state_of_targets[target] == -1
 
     def get_action(self, idx):
         return possible_actions()[idx]
 
-    # TODO: not implemented, does not take into account the dummy nodes in
-    # original benchmark.
+    # TODO: not implemented
     def outcome_probabilities(self, name, action):
+        pass
 
-        dist = dict()
+    """
+         decreases counters on orders by _cons_, any orders that reach zero and
+         are not sufficiently close (<= self.radius), are refreshed to the
+         period.
 
-        state_id = self.name_to_state(name)
-
-        # ActionData... source target, consumption, distr
-        action_iterator = self.env.consmdp.actions_for_state(state_id)
-
-        # the iterator does not have random access, pull out the action like this
-        action_data = next(islice(action_iterator, action, None))
-
-        for x in action_data.distr.keys():
-            dist[self.state_to_name(x)] = float(action_data.distr[x])
-
-        return dist
-    # decreases counters on orders by _cons_, any orders that reach zero and
-    # are not sufficiently close (< self.radius), are refreshed as well
-    # returns a flag signalizing that the agent can accept an order
+         returns true if a customer is sufficiently close and his order can be accepted,
+         this moves the environment into a decision_node dummy state next step.
+    """
     def decrease_ctrs(self, cons):
         orders_available = False
+
         for t in self.targets:
             if self.target_active(t):
                 continue
 
             self.state_of_targets[t] -= cons
-
             if self.state_of_targets[t] <= 0:
                 dist = self.distance_to_target(t)
-                new_value = self.periods[t]
+                new_value = self.period
 
                 if dist <= self.radius:
                     new_value = 0
@@ -196,7 +199,7 @@ class ManhattanEnv:
     def reload_ctrs(self):
         for t in self.targets:
             if self.state_of_targets[t] == 0:
-                self.state_of_targets[t] = self.periods[t]
+                self.state_of_targets[t] = self.period
 
 
     def distance_to_target(self, target):
@@ -206,7 +209,7 @@ class ManhattanEnv:
 
             # check if last known gps position is initialized
             if self.last_gps is None:
-                return []
+                return inf
 
             # use last known gps position
             else:
@@ -216,7 +219,7 @@ class ManhattanEnv:
         else:
             state_data = self.geo_data[self.position]
             self.last_gps = self.position
-            
+
         state_lat = float(state_data['lat'])
         state_lon = float(state_data['lon'])
 
@@ -228,12 +231,7 @@ class ManhattanEnv:
 
 
     def play_action(self, action):
-
-        # record relevant information (used for animating trajectory)
-        self.history.append( (self.position, self.state_of_targets, self.decision_node) )
-
-
-        # if in decision node, handle (potential) acceptance of an order
+        # if in decision node, handle (potential) orders
         if self.decision_node:
             self.decision_node = False
 
@@ -242,10 +240,13 @@ class ManhattanEnv:
                 # accept target
                 self.state_of_targets[self.targets[action]] = -1
             self.reload_ctrs()
+
             return (self.position, self.state_of_targets, self.decision_node), 0, 0, self.is_over()
 
         # otherwise, proceed by moving in the underlying cmdp, recording
-        # reward/penalty and adjusting periods of orders
+        # reward/penalty and adjusting periods of orders, record the state into
+        # history as well
+        self.history.append( self.position )
 
         # get linked list of actions from cmdp
         state_id = self.name_to_state(self.position)
@@ -257,7 +258,7 @@ class ManhattanEnv:
         # get successor
         next_state = np.random.choice(list(action_data.distr.keys()),
                                       p=list(action_data.distr.values()))
-        
+
         # skip dummy state, record penalty
         action_iterator = self.env.consmdp.actions_for_state(next_state)
         action_data = next(islice(action_iterator, 0, None))
@@ -269,18 +270,28 @@ class ManhattanEnv:
         if self.capacity > 0:
             self.energy -= action_data.cons
 
+        # if an order is active, add to delay
+        if self.currently_delivering():
+            self.order_delay += action_data.cons
+
         # decrease counters for targets
-        # TODO: maybe clip the consumption to a predefined range like [1, 2, 3]
         self.decision_node = self.decrease_ctrs(action_data.cons)
 
-        # penalize if consumption is past threshold and some target is active
-        penalty = (action_data.cons >= self.cons_thd) * ( len([1 for t in self.targets if self.state_of_targets[t] == -1]) > 0 )
+        penalty = 0
+        # penalize for delay
+        if self.order_delay > self.cons_thd:
+            penalty = 1
+            self.order_delay = 0
 
-        # reward if target is reached
+        # reward if order is delivered
         reward = 0
         if (self.position in self.targets) and (self.state_of_targets[self.position] == -1):
             reward = 1
-            self.state_of_targets[self.position] = self.periods[self.position]
+            self.state_of_targets[self.position] = self.period
+
+            # if this was the only order, set order delay back to zero
+            if not self.currently_delivering():
+                self.order_delay = 0
 
         return (self.position, self.state_of_targets, self.decision_node), float(reward), float(penalty), self.is_over()
 
@@ -290,36 +301,28 @@ class ManhattanEnv:
         return self.capacity > 0 and self.energy <= 0
 
     def make_checkpoint(self, checkpoint_id):
-        history_copy = copy.deepcopy(self.history)
         counter_copy = copy.deepcopy(self.state_of_targets)
-
-        self.histories[checkpoint_id] = history_copy
-        self.checkpoints[checkpoint_id] = (self.position, self.last_gps, self.energy, counter_copy, self.decision_node)
+        self.checkpoints[checkpoint_id] = (self.position, self.last_gps, self.energy, self.order_delay, counter_copy, self.decision_node)
+        self.histories[checkpoint_id] = len(self.history)
 
     def restore_checkpoint(self, checkpoint_id):
-        self.history = self.histories[checkpoint_id]
-        self.position, self.last_gps, self.energy, self.state_of_targets, self.decision_node = self.checkpoints[checkpoint_id]
+        self.position, self.last_gps, self.energy, self.order_delay, self.state_of_targets, self.decision_node = self.checkpoints[checkpoint_id]
+        history_id = self.histories[checkpoint_id]
+        self.history = self.history[:history_id]
 
     def reset(self):
         self.energy = self.capacity
         self.position = self.init_state
-        self.state_of_targets = { t : self.periods[t] for t in self.targets }
+        self.state_of_targets = { t : self.period for t in self.targets }
         self.decision_node = False
-
-        self.histories.clear()
+        
         self.checkpoints.clear()
         self.history = []
 
     """
-        taken from AEVEnv
-
-        call after final execution of the policy, i.e. 
-        after calling play_action() as many times as you wish
+        Visualizes the positions in self.history on the map of manhattan.
+        Taken directly from AEVEnv
     """
-
-    # TODO:
-    # need to call reset beforehand to clear the history, afterwards this
-    # should work fine.
     # interval signals number of frames between each animation plot
     def animate_simulation(self, interval=100, filename="map.html"):
         """
@@ -330,13 +333,13 @@ class ManhattanEnv:
         init_state = self.init_state
 
         def is_int(s):
-            try: 
+            try:
                 int(s)
                 return True
             except ValueError:
                 return False
 
-        
+
         # Load NYC Geodata
         for _, _, data in self.G.edges(data=True, keys=False):
             data['time_mean'] = float(data['time_mean'])
@@ -351,11 +354,11 @@ class ManhattanEnv:
         trajectory = []
 
         # filter dummy states
-        for position, targets, decision  in self.history:
+        for position  in self.history:
             if position not in list(self.G.nodes):
                 pass
             else:
-                trajectory.append( (position, targets, decision) )
+                trajectory.append( position )
 
         # create baseline map
         nodes_all = {}
@@ -370,8 +373,8 @@ class ManhattanEnv:
         min_point = [min(global_lat), min(global_lon)]
         max_point =[max(global_lat), max(global_lon)]
         m = folium.Map(zoom_start=1, tiles='cartodbpositron')
-        m.fit_bounds([min_point, max_point])        
-            
+        m.fit_bounds([min_point, max_point])
+
         # add initial state, reload states and target states
         folium.CircleMarker(location=[self.G.nodes[init_state]['lat'], self.G.nodes[init_state]['lon']],
                         radius= 3,
@@ -379,7 +382,7 @@ class ManhattanEnv:
                         color='green',
                         fill_color = 'green',
                         fill_opacity=1,
-                        fill=True).add_to(m)        
+                        fill=True).add_to(m)
 
         for node in targets:
             folium.CircleMarker(location=[self.G.nodes[node]['lat'], self.G.nodes[node]['lon']],
@@ -398,7 +401,7 @@ class ManhattanEnv:
         for pair in path:
 
             # pull out positions from the history tuple
-            pos1, pos2 = pair[0][0], pair[1][0]
+            pos1, pos2 = pair
 
             t_edge = 1
             lines.append(dict({'coordinates':
@@ -427,7 +430,7 @@ class ManhattanEnv:
         positions = [{
             'type': 'Feature',
             'geometry': {
-                        'type':'Point', 
+                        'type':'Point',
                         'coordinates':position['coordinates']
                         },
             'properties': {
@@ -446,29 +449,6 @@ class ManhattanEnv:
         data_lines = {'type': 'FeatureCollection', 'features': features}
         data_positions = {'type': 'FeatureCollection', 'features': positions}
         folium.plugins.TimestampedGeoJson(data_lines,  transition_time=interval,
-                               period='PT1S', add_last_point=False, date_options='mm:ss', duration=None).add_to(m)      
+                               period='PT1S', add_last_point=False, date_options='mm:ss', duration=None).add_to(m)
 
         m.save(filename)
-
-if __name__ == "__main__":
-
-    # pretty close starting state for easy testing
-    init_state = '42429690'
-
-    # three states next to each other for default orders
-    targets = ['42455666', '42442977', '596775930']
-
-    # periods 
-    periods = { target : 50 for target in targets }
-
-    # higher period for last target
-    periods[targets[-1]] = 100
-    x = ManhattanEnv(targets, init_state, periods, capacity)
-
-    for i in range(30):
-        print(x.current_state())
-        print(x.possible_actions())
-        a = x.possible_actions()[-1]
-        s, r, p, o = x.play_action(a)
-        print(r, p, o)
-    x.animate_simulation(interval=100)
